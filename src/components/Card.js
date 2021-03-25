0fd4292099121772061aec6e10cec47ff15ab055
Christina <christinaibailey@live.com>
2021-03-25 17:30:40 +0100

    feat: styling done

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,10 @@ import CardBS from "react-bootstrap/Card";
 
 export function Card({ pokemon }) {
   return (
-    <CardBS>
-      <CardBS.Title>{pokemon.name}</CardBS.Title>
+    <CardBS style={{ cursor: "pointer" }}>
+      <CardBS.Title style={{ padding: "20px", margin: "0" }}>
+        {pokemon.name.toUpperCase()}
+      </CardBS.Title>
     </CardBS>
   );
 }
--- src/components/FeatPokemon.js
+++ src/components/FeatPokemon.js
@@ -1,35 +1,36 @@
 import { Modal, Button, Image } from "react-bootstrap";
-// import Button from "react-bootstrap/Button";
-// import Image from "react-bootstrap/Image";
-
-import "bootstrap/dist/css/bootstrap.min.css";
 
 export function FeatPokemon({ featPokemon, onHide }) {
-  console.log(featPokemon, "here");
-
   if (featPokemon) {
     return (
       <Modal
         show
-        size="lg"
+        size="md"
         aria-labelledby="contained-modal-title-vcenter"
         centered
       >
         <Modal.Header>
-          <Modal.Title id="contained-modal-title-vcenter">
-            {featPokemon.name.toUpperCase()}{" "}
+          <div style={{ float: "left" }}>
+            <div style={{ fontSize: "25px" }}>
+              {featPokemon.name.toUpperCase()}{" "}
+            </div>{" "}
             <b>XP: {featPokemon.base_experience}</b>
-            <div onClick={onHide}>x</div>
-          </Modal.Title>
+          </div>
+
+          <Button variant="danger" onClick={onHide}>
+            Close
+          </Button>
         </Modal.Header>
-        <Modal.Body>
-          <Image src={featPokemon.sprites.front_default} rounded />
-
-          <h4>Abilities</h4>
-
-          {featPokemon.abilities.map((oneAbility) => (
-            <p> {oneAbility.ability.name}</p>
-          ))}
+        <Modal.Body style={{ alignSelf: "center" }}>
+          <div style={{ float: "left", padding: "10px" }}>
+            <Image src={featPokemon.sprites.front_default} rounded />
+          </div>
+          <div style={{ float: "right", padding: "10px" }}>
+            <h4>Abilities:</h4>
+            {featPokemon.abilities.map((oneAbility) => (
+              <p> + {oneAbility.ability.name}</p>
+            ))}
+          </div>
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={onHide}>Add to Favorites</Button>
