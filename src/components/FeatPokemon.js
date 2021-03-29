3418ebf377541f5c55b4846eb2e2a13ab25446a0
Christina <christinaibailey@live.com>
2021-03-29 16:04:07 +0200

    feat: add redux

--- src/components/FeatPokemon.js
+++ src/components/FeatPokemon.js
@@ -1,7 +1,20 @@
 import { Modal, Button, Image } from "react-bootstrap";
+import { connect } from "react-redux";
+import updateFeatPokemon from "../store/actions/updateFeatPokemon";
+import addFavoritePokemon from "../store/actions/addFavoritePokemon";
 
-export function FeatPokemon({ featPokemon, onHide }) {
-  if (featPokemon) {
+function FeatPokemon(props) {
+  function onClose(event) {
+    event.preventDefault();
+    props.updateFeatPokemon(false);
+  }
+
+  function addToFav(event) {
+    event.preventDefault();
+    props.addFavoritePokemon(props.featPokemon.name);
+  }
+
+  if (props.featPokemon) {
     return (
       <Modal
         show
@@ -12,31 +25,47 @@ export function FeatPokemon({ featPokemon, onHide }) {
         <Modal.Header>
           <div style={{ float: "left" }}>
             <div style={{ fontSize: "25px" }}>
-              {featPokemon.name.toUpperCase()}{" "}
+              {props.featPokemon.name.toUpperCase()}{" "}
             </div>{" "}
-            <b>XP: {featPokemon.base_experience}</b>
+            <b>XP: {props.featPokemon.base_experience}</b>
           </div>
 
-          <Button variant="danger" onClick={onHide}>
+          <Button variant="danger" onClick={(e) => onClose(e)}>
             Close
           </Button>
         </Modal.Header>
         <Modal.Body style={{ alignSelf: "center" }}>
           <div style={{ float: "left", padding: "10px" }}>
-            <Image src={featPokemon.sprites.front_default} rounded />
+            <Image src={props.featPokemon.sprites.front_default} rounded />
           </div>
           <div style={{ float: "right", padding: "10px" }}>
             <h4>Abilities:</h4>
-            {featPokemon.abilities.map((oneAbility) => (
-              <p> + {oneAbility.ability.name}</p>
+            {props.featPokemon.abilities.map((oneAbility, i) => (
+              <p key={i}> + {oneAbility.ability.name}</p>
             ))}
           </div>
         </Modal.Body>
         <Modal.Footer>
-          <Button onClick={onHide}>Add to Favorites</Button>
+          <Button onClick={(e) => addToFav(e)}>Add to Favorites</Button>
         </Modal.Footer>
       </Modal>
     );
   }
   return false;
 }
+
+const MapStateToProps = (state) => {
+  return {
+    featPokemon: state.featPokemon,
+    favPokemon: state.favPokemon,
+  };
+};
+const MapDispatchToProps = (dispatch) => {
+  return {
+    updateFeatPokemon: (pokemonName) =>
+      dispatch(updateFeatPokemon(pokemonName)),
+    addFavoritePokemon: (pokemonName) =>
+      dispatch(addFavoritePokemon(pokemonName)),
+  };
+};
+export default connect(MapStateToProps, MapDispatchToProps)(FeatPokemon);
